8bd568d16232fc3e4e483594786f3a7bc17b657f
GGGuang12138 <ggguang12138@icloud.com>
2022-03-09 18:47:30 +0800

    补充上一个提交

--- msg-handler/src/main/java/com/gg/msg/handler/receiver/Receiver.java
+++ msg-handler/src/main/java/com/gg/msg/handler/receiver/Receiver.java
@@ -3,10 +3,14 @@ package com.gg.msg.handler.receiver;
 import com.alibaba.fastjson.JSON;
 import com.gg.msg.domain.TaskInfo;
 import com.gg.msg.handler.handler.SmsHandler;
+import com.gg.msg.handler.utils.GroupIdMappingUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 import java.io.InputStream;
@@ -26,15 +30,18 @@ public class Receiver {
     @Autowired
     private SmsHandler smsHandler;
 
-    @KafkaListener(topics = {"unifiedMsg"}, groupId = "sms")
-    public void consumer(ConsumerRecord<?, String> consumerRecord) {
+    @KafkaListener(topics = {"unifiedMsg"})
+    public void consumer(ConsumerRecord<?, String> consumerRecord, @Header(KafkaHeaders.GROUP_ID) String groupId) {
         Optional<String> kafkaMessage = Optional.ofNullable(consumerRecord.value());
         if (kafkaMessage.isPresent()) {
             List<TaskInfo> lists = JSON.parseArray(kafkaMessage.get(), TaskInfo.class);
-            for (TaskInfo taskInfo : lists) {
-                smsHandler.doHandler(taskInfo);
+            String messageGroupId = GroupIdMappingUtils.getGroupIdByTaskInfo(lists.get(0));
+            if (groupId.equals(messageGroupId)){
+                for (TaskInfo taskInfo : lists) {
+                    smsHandler.doHandler(taskInfo);
+                }
+                log.info("receiver message:{}", JSON.toJSONString(lists));
             }
-            log.info("receiver message:{}", JSON.toJSONString(lists));
         }
 
     }
